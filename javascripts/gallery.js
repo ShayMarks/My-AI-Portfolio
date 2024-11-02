56faab5883decde65dbf10dfd4f3c38f91a5f1fd
Shay Mrks <135506027+ShayMarks@users.noreply.github.com>
2024-11-02 19:36:27 +0200

    update

--- javascripts/gallery.js
+++ javascripts/gallery.js
@@ -12,7 +12,8 @@ function openGalleryModal(galleryIndex) {
 
   var imagesPaths = [
     ["images/harabotBarzel/war1.png", "images/harabotBarzel/war2.png","images/harabotBarzel/war3.png","images/harabotBarzel/war4.png","images/harabotBarzel/war5.png","images/harabotBarzel/war6.png","images/harabotBarzel/war7.png","images/harabotBarzel/war8.png"],
-    ["images/cyberPunk/cyberpunk.png", "images/cyberPunk/cyberpunk1.png", "images/cyberPunk/cyberpunk2.png", "images/cyberPunk/cyberpunk3.png", "images/cyberPunk/cyberpunk4.png", "images/cyberPunk/cyberpunk5.png", "images/cyberPunk/cyberpunk6.png", "images/cyberPunk/cyberpunk7.png", "images/cyberPunk/cyberpunk8.png", "images/cyberPunk/cyberpunk9.png", "images/cyberPunk/cyberpunk10.png"]
+    ["images/cyberPunk/cyberpunk.png", "images/cyberPunk/cyberpunk1.png", "images/cyberPunk/cyberpunk2.png", "images/cyberPunk/cyberpunk3.png", "images/cyberPunk/cyberpunk4.png", "images/cyberPunk/cyberpunk5.png", "images/cyberPunk/cyberpunk6.png", "images/cyberPunk/cyberpunk7.png", "images/cyberPunk/cyberpunk8.png", "images/cyberPunk/cyberpunk9.png", "images/cyberPunk/cyberpunk10.png"],
+    ["images/Independence/1.png","images/Independence/2.png","images/Independence/3.png","images/Independence/4.png","images/Independence/5.png","images/Independence/6.png","images/Independence/7.png","images/Independence/8.png","images/Independence/9.png","images/Independence/10.png",]
   ];
 
   imagesPaths[galleryIndex].forEach(function(path) {
